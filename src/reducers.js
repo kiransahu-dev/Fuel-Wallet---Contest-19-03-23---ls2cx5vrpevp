d769538a7d09e5359006a256a10c31c87f8a4b54
kiransahu-dev <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-19 13:53:29 +0530

    completed reducer.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,26 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "AddUser":
+      const newUser = {
+        id: uuidv4(),
+        name: action.payload,
+        balance: 10,
+      };
+      return {
+        ...state,
+        users: [...state.users, newUser],
+      };
+    case "Wallet":
+      const newBalance = state.walletBalance -50;
+      return {
+        ...state,
+        walletBalance: newBalance >= 0 ? newBalance : state.walletBalance,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
